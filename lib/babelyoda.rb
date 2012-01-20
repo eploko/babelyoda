6162d5dcbe9868ca723402b61e9a837a761f1693
Andrey Subbotin <andrey@subbotin.me>
2012-01-20 19:11:42 +0400

    Verification fix

--- lib/babelyoda.rb
+++ lib/babelyoda.rb
@@ -201,29 +201,34 @@ namespace :babelyoda do
         dev_lang_strings = Babelyoda::Strings.new(filename, spec.development_language).read
         combined_keyset.merge!(dev_lang_strings)
         spec.localization_languages.each do |language|
-          lang_strings = Babelyoda::Strings.new(filename, language).read
+          lang_strings = Babelyoda::Strings.new(File.localized(filename, language), language).read
           combined_keyset.merge!(lang_strings)
         end
       end
-      $logger.info "#{spec.development_language}: #{combined_keyset.keys.size} keys"
-      total_missing_count = combined_keyset.keys.size
-      
-      present = {}
-      spec.localization_languages.each do |language|
-        present[language] = Babelyoda::Keyset.new("babelyoda.verify.#{language}")
-      end
       combined_keyset.drop_empty!
+      
+      count = {}
+      spec.all_languages.each { |lang| count[lang.to_sym] = 0}
+      
       combined_keyset.keys.each_value do |key|
-        spec.localization_languages.each do |lang|
-          present[lang.to_sym].merge_key!(key) if key.values.has_key?(lang.to_sym)
+        spec.all_languages.each do |lang|
+          count[lang.to_sym] += 1 if key.values.has_key?(lang.to_sym)
         end
       end
+
+      total_count = count[spec.development_language.to_sym]
+      $logger.info "#{spec.development_language}: #{total_count} keys"
+      
+      all_found = true
       spec.localization_languages.each do |language|
-        count = present[language].keys.size
-        total_missing_count -= count
-        $logger.error "#{language}: #{combined_keyset.keys.size - count} keys (#{count} translations missing)" if count > 0
+        lang_count = count[language.to_sym]
+        missing_count = total_count - lang_count
+        if missing_count > 0
+          $logger.error "#{language}: #{lang_count} keys (#{missing_count} translations missing)"
+          all_found = false
+        end
       end
-      exit 1 if total_missing_count > 0
+      exit 1 unless all_found
     end
     
     namespace :remote do
--- lib/babelyoda/keyset.rb
+++ lib/babelyoda/keyset.rb
@@ -1,3 +1,5 @@
+require_relative 'logger'
+
 module Babelyoda
   class Keyset
     attr_accessor :name
